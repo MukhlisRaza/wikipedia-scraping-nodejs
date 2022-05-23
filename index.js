2d58b1b5cfea46368a7bcc0c8ba5a1a52854a31c
Mukhlis Raza <68111775+MukhlisRaza@users.noreply.github.com>
2022-05-23 13:17:08 +0500

    install express.js

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const cheerio = require("cheerio");
-const rp = require("request-promise");
+var express = require("express");
 var fs = require("fs");
-const prompt = require("prompt-sync")();
+const rp = require("request-promise");
+var cheerio = require("cheerio");
+var app = express();
 
-const name = prompt("Enter link here!");
+// const name = prompt("Enter link here!");
 
-// const reqLink = `https://simple.wikipedia.org/wiki/List_of_presidents_of_the_United_States`;
+const reqLink = `https://simple.wikipedia.org/wiki/List_of_presidents_of_the_United_States`;
 
-const reqLink = `${name}`;
+// const reqLink = `${name}`;
+app.get("/scrape", function (req, res) {
+  rp(reqLink)
+    .then(function (html) {
+      // Process html...
+      // console.log(html);
+      const $ = cheerio.load(html);
+      // console.log($);
+      const title = $(".wikitable td b a");
+      const wiki = [];
 
-rp(reqLink)
-  .then(function (html) {
-    // Process html...
-    // console.log(html);
-    const $ = cheerio.load(html);
-    // console.log($);
-    const title = $(".wikitable td b a");
-    const wiki = [];
-
-    for (let i = 0; i < title.length; i++) {
-      const Name = title[i].attribs.title;
-      const Link = title[i].attribs.href;
-      wiki.push({ Name, Link });
-    }
-    // console.log(wiki);
-    fs.writeFile("output.json", JSON.stringify(wiki), function (err) {
-      console.log(
-        "Sraping data successfully written! - Check your project public/output.json file"
-      );
+      for (let i = 0; i < title.length; i++) {
+        const Name = title[i].attribs.title;
+        const Link = title[i].attribs.href;
+        wiki.push({ Name, Link });
+      }
+      // console.log(wiki);
+      fs.writeFile("output.json", JSON.stringify(wiki), function (err) {
+        console.log(
+          "Sraping data successfully written! - Check your project public/output.json file"
+        );
+      });
+    })
+    .then((res) => console.log(res))
+    .catch(function (err) {
+      // Crawling failed...
+      console.log(err);
     });
-  })
-  .then((res) => console.log(res))
-  .catch(function (err) {
-    // Crawling failed...
-    console.log(err);
-  });
+});
+
+app.listen("8001");
+
+console.log("Your node server start successfully....");
+
+exports = module.exports = app;
